e277fd29ec9c2b4c9f515132bb305e48200b843f
lauramagf <1598880@sga.pucminas.br>
2025-06-17 14:11:40 -0300

    Update scripts.js
    permitindo que o usuario adicione uma foto de perfil

--- src/perfil/scripts.js
+++ src/perfil/scripts.js
@@ -8,7 +8,7 @@ alternar.addEventListener("click", function () {
 
 }) 
 
-// Função de salvar
+// 
 const salvarAlt = document.getElementById("salvar");
 
 salvarAlt.addEventListener("mousedown", function () {
@@ -16,3 +16,23 @@ salvarAlt.addEventListener("mousedown", function () {
     salvou.innerHTML = "Salvo!"
     salvou.style.background ="rgb(74, 157, 74)"
 })
+
+// permitindo que o usuário adicione foto de perfil
+const editarIcone = document.getElementById('editar');
+const inputFoto = document.getElementById('foto');
+const imagemUser = document.getElementById('user');
+
+editarIcone.addEventListener('click', function () {
+    inputFoto.click(); // abre o seletor de arquivos
+});
+
+inputFoto.addEventListener('change', function () {
+    const arquivo = this.files[ 0 ];
+    if (arquivo) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            imagemUser.src = e.target.result;
+        };
+        reader.readAsDataURL(arquivo);
+    }
+});
